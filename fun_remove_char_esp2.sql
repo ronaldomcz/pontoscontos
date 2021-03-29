8043d4a75c69a53c9edd707b50dbbd1dafcbeadc
ronaldofilho87 <ronaldo7soul@gmail.com>
2021-03-29 17:18:54 -0300

    teste

---
+++ fun_remove_char_esp2.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  Arquivo criado - Segunda-feira-Março-29-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUN_REMOVE_CHAR_ESP2
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "PONTOSCONTOS"."FUN_REMOVE_CHAR_ESP2" (texto in varchar2) return varchar2 is
+
+    begin
+      return translate(texto,'ÑÁÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕÄËÏÖÜÇñáéíóúàèìòùâêîôûãõäëïöüç.-!"''`#$%().:[/]{}¨+?;ºª°§*<>','NAEIOUAEIOUAEIOUAOAEIOUCnaeiouaeiouaeiouaoaeiouc');
+    end;
+
+/
